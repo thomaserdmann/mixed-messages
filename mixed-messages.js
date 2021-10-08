7290f042acc844af9058f74dfc97d3912da9f2bd
Thomas Erdmann <thomas@innofy.nl>
2021-10-08 09:52:37 +0200

    Create functions for random values

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,16 +1,29 @@
-const months = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December',
-];
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const drinks = ['beer', 'wine', 'whiskey', 'rum', 'coffee', 'milk', 'water', 'soda', 'cocktails', 'thee', 'lemonade'];
+const randomMonth = () => {
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	return months[Math.floor(Math.random() * months.length)];
+};
+const randomDay = () => {
+	const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	return days[Math.floor(Math.random() * days.length)];
+};
+const randomDrink = () => {
+	const drinks = ['beer', 'wine', 'whiskey', 'rum', 'coffee', 'milk', 'water', 'soda', 'cocktails', 'thee', 'lemonade'];
+	return drinks[Math.floor(Math.random() * drinks.length)];
+};
+
+console.log(randomMonth());
+console.log(randomDay());
+console.log(randomDrink());
