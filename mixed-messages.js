118e37ba8bf3406fd1552792391dc2d4e6c62c3a
Thomas Erdmann <thomas@innofy.nl>
2021-10-08 09:43:52 +0200

    Init data

---
+++ mixed-messages.js
@@ -0,0 +1,16 @@
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const drinks = ['beer', 'wine', 'whiskey', 'rum', 'coffee', 'milk', 'water', 'soda', 'cocktails', 'thee', 'lemonade'];
